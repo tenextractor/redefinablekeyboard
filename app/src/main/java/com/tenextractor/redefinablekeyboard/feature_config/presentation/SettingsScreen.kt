03b65b5475cdddf5d51b959a95fe5023fba0a189
tenextractor <139619642+tenextractor@users.noreply.github.com>
2024-09-12 19:28:45 +0530

    update settings screen

--- app/src/main/java/com/tenextractor/redefinablekeyboard/feature_config/presentation/SettingsScreen.kt
+++ app/src/main/java/com/tenextractor/redefinablekeyboard/feature_config/presentation/SettingsScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tenextractor.redefinablekeyboard.R
 import com.tenextractor.redefinablekeyboard.feature_config.SharedPrefsManager
 import com.tenextractor.redefinablekeyboard.feature_config.HapticFeedbackService
@@ -22,6 +24,35 @@ fun SettingsScreen() {
     var hapticFeedbackEnabled by remember { mutableStateOf(sharedPrefsManager.isHapticFeedbackEnabled()) }
     var errorMessage by remember { mutableStateOf<String?>(null) }
 
+    Column {
+        TopAppBar({ Text(stringResource(R.string.settings)) })
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                stringResource(R.string.haptic_feedback),
+                Modifier.padding(15.dp),
+                fontSize = 20.sp,
+                color = Color.White
+            )
+            Switch(
+                modifier = Modifier.padding(end = 15.dp),
+                checked = hapticFeedbackEnabled,
+                onCheckedChange = {
+                    hapticFeedbackEnabled = it
+                    sharedPrefsManager.setHapticFeedbackEnabled(it)
+                    if (it) {
+                        hapticFeedbackService.performHapticFeedback()
+                    }
+                }
+            )
+        }
+    }
+/*
     Scaffold(
         topBar = {
             TopAppBar(
@@ -55,5 +86,5 @@ fun SettingsScreen() {
                 )
             }
         }
-    }
+    }*/
 }
