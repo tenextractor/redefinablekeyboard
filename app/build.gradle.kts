d5f248ed8ca9eda7bdd72ff07a89e6ff1b6f64e2
tenextractor <139619642+tenextractor@users.noreply.github.com>
2024-11-17 17:06:39 +0530

    Update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.9.0.20241105"
+        versionName = "0.10.0.20241117"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
